7bd2360ba2cbdf28fd221f69b1fd9c76d67c12b0
Andrew D'Angelo <dangeloandrew@outlook.com>
2024-04-06 14:29:48 -0500

    Draw meta mode

--- src/Overlay.SDL.cpp
+++ src/Overlay.SDL.cpp
@@ -6,6 +6,7 @@
 #include <sys/ioctl.h>
 
 #include <stdexcept>
+#include <cstring>
 #include <memory>
 #include <unordered_map>
 
@@ -15,10 +16,19 @@
 
 using namespace std::literals;
 
+struct OverlayStorage
+{
+	int x, y;
+	size_t width, height;
+	std::unique_ptr<unsigned char[]> pix;
+};
+
 using UniqueWindow = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
 using UniqueRenderer = std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)>;
+
 static std::unordered_map<int, UniqueWindow> g_windows;
 static std::unordered_map<int, UniqueRenderer> g_renderers;
+static std::unordered_map<void*, OverlayStorage> g_overlayStorages;
 
 SharpSession::SharpSession(char const* sharp_dev)
 	: m_fd{(int)g_windows.size()}
@@ -85,11 +95,19 @@ int SharpSession::get()
 }
 
 Overlay::Overlay(SharpSession& session,
-	int x, int y, size_t width, size_t height, unsigned char const* pix)
+	int x, int y, size_t width, size_t height, unsigned char const* pix_)
 	: m_session{session}
-	, m_storage{nullptr}
-	, m_display{nullptr}
-{}
+	, m_storage{(void*)this}
+	, m_display{}
+{
+	if (x < 0) { x = 400 + x; }
+	if (y < 0) { y = 240 + y; }
+
+	auto pix = std::make_unique<unsigned char[]>(height * width);
+	std::memcpy(pix.get(), pix_, height * width);
+	g_overlayStorages.emplace(m_storage, OverlayStorage{
+		x, y, width, height, std::move(pix)});
+}
 
 Overlay::Overlay(Overlay&& expiring)
 	: m_session{expiring.m_session}
@@ -101,13 +119,26 @@ Overlay::Overlay(Overlay&& expiring)
 }
 
 Overlay::~Overlay()
-{}
+{
+	g_overlayStorages.erase(m_storage);
+}
 
 void Overlay::show()
 {
 	auto renderer = g_renderers.at(m_session.get()).get();
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderDrawLine(renderer, 50, 120, 350, 120);
+	auto& os = g_overlayStorages.at(m_storage);
+
+	for (auto y = os.y; y < os.y + os.height; y++) {
+		for (auto x = os.x; x < os.x + os.width; x++) {
+
+			auto px = (os.pix.get()[((y - os.y) * os.width) + (x - os.x)])
+				? 255
+				: 0;
+			SDL_SetRenderDrawColor(renderer, px, px, px, 255);
+			SDL_RenderDrawPoint(renderer, x, y);
+		}
+	}
+
 	SDL_RenderPresent(renderer);
 }
 
